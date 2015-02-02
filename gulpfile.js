8a08bb9fea86c130fe7e022bc6cd51c3fd17e65c
Yogev Ahuvia <yogev.ahuvia@gmail.com>
2015-02-02 11:35:10 +0200

    Added SASS and AutoPrefixer, restored skrollr.js

--- gulpfile.js
+++ gulpfile.js
@@ -1,11 +1,13 @@
-var gulp        = require('gulp'),
-    order       = require('gulp-order'),
-    concat      = require('gulp-concat'),
-    uglify      = require('gulp-uglify'),
-    minifyCSS = require('gulp-minify-css'),
-    serve      = require('gulp-serve'),
-    liveReload  = require('gulp-livereload'),
-    rename      = require('gulp-rename');
+var gulp         = require('gulp'),
+    order        = require('gulp-order'),
+    concat       = require('gulp-concat'),
+    uglify       = require('gulp-uglify'),
+    minifyCSS    = require('gulp-minify-css'),
+    serve        = require('gulp-serve'),
+    liveReload   = require('gulp-livereload'),
+    sass         = require('gulp-sass'),
+    autoprefixer = require('gulp-autoprefixer'),
+    rename       = require('gulp-rename');
 
 gulp.task('uglify', function() {
     return gulp.src(['js/**/*.js'])
@@ -21,8 +23,20 @@ gulp.task('uglify', function() {
         .pipe(gulp.dest('dist/js'))
         .pipe(liveReload());
 });
-gulp.task('minify-css', function(){
+
+gulp.task('styles', function() {
+  return gulp.src('sass/*.scss')
+    .pipe(sass({ style: 'expanded' }))
+    .pipe(autoprefixer('last 2 version', 'safari 5', 'ie 8', 'ie 9', 'opera 12.1'))
+    .pipe(gulp.dest('css'))
+});
+
+gulp.task('minify-css', ['styles'], function(){
     return gulp.src('css/**/*.css')
+        .pipe(order([
+          "vendor/*.css",
+          "*.css"
+        ]))
         .pipe(concat('fedsonslack'))
         .pipe(minifyCSS({keepBreaks:true}))
         .pipe(rename({
@@ -39,7 +53,7 @@ gulp.task('serve', serve({
 gulp.task('watch', function() {
     liveReload.listen({start:true});
 
-    gulp.watch('css/**/*.css' , ['minify-css']);
+    gulp.watch(['sass/**/*.scss', 'css/**/*.css'] , ['minify-css']);
     gulp.watch('js/**/*.js',  ['uglify']);
     gulp.watch('index.html', liveReload)
 
